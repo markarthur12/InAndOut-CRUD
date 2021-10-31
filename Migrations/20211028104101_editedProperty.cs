c87732dfb0cbc6929960bc47305be7ee4bf74d79
Mark Arthur <markarthurgabiana@gmail.com>
2021-10-31 12:40:25 +0400

    Initial commit

---
+++ Migrations/20211028104101_editedProperty.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class editedProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpenseTypes",
+                table: "ExpenseTypes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ExpenseTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ExpenseTypes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpenseTypes",
+                table: "ExpenseTypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
